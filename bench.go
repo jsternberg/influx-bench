e2f4f4c2fac0e476c6f889f9cedde9ea990f906c
Jonathan A. Sternberg <jonathan@influxdb.com>
2016-11-03 13:01:09 -0500

    Use a different RNG per point generator
    Seed these generators with the global RNG so that they are consistent
    and different.

--- bench.go
+++ bench.go
@@ -11,6 +11,8 @@ type PointGenerator struct {
 	Point     influxdb.Point
 	C         chan influxdb.Point
 	StartTime time.Time
+
+	rng *rand.Rand
 }
 
 func NewPointGenerator(pt influxdb.Point, ch chan influxdb.Point, start time.Time) *PointGenerator {
@@ -18,13 +20,14 @@ func NewPointGenerator(pt influxdb.Point, ch chan influxdb.Point, start time.Tim
 		Point:     pt,
 		C:         ch,
 		StartTime: start,
+		rng:       rand.New(rand.NewSource(rand.Int63())),
 	}
 }
 
 func (g *PointGenerator) GeneratePoints(pointN int, interval time.Duration) {
 	now := g.StartTime
 	for i := 0; i < pointN; i++ {
-		g.Point.Fields = influxdb.Value(rand.Int63())
+		g.Point.Fields = influxdb.Value(g.rng.Int63())
 		g.Point.Time = now
 		g.C <- g.Point
 		now = now.Add(interval)
